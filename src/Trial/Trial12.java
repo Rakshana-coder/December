1e4ce108780e40a2547eef32e4c08a329990fdcd
Rakshana B <rakshana.b1994@gmail.com>
2025-12-26 08:41:57 +0530

    push the code

---
+++ src/Trial/Trial12.java
@@ -0,0 +1,26 @@
+package Trial;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Trial12 {
+
+    public static void main (String[] args) {
+
+        int[] array = {1,2,2,2,3,3,3,4,5,6,7};
+
+        Map<Integer, Integer> duplicates = new HashMap<>();
+
+        for(int num:array) {
+
+            if(duplicates.containsKey(num)) {
+
+                duplicates.put(num, duplicates.get(num) + 1);
+            }
+                else
+                duplicates.put(num, 1);
+            }
+            System.out.println(duplicates);
+        }
+    }
+
